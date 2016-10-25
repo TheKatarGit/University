fa831884aca478f9330eb6a5d25b508ed8427239
TheKatarGit <d.mihalkovich@gmail.com>
2016-10-25 09:30:20 +0300

    Fixed buffer implementation

--- C Programming/Lab 4/Lab 4 part 2/flowlib.c
+++ C Programming/Lab 4/Lab 4 part 2/flowlib.c
@@ -5,8 +5,9 @@ void rearrange(char *array, int letter, int len) {
         *array = letter;
         return;
     }
-    for (size_t i = 0; i < len - 1; i++) {
+    for (size_t i = 0; i <= len - 1; i++) {
         *(array + i) = *(array + i + 1);
+
     }
     *(array + len) = letter;
 }
@@ -19,24 +20,39 @@ void go_through(FILE *inFile, const char *replacee, const char *replacer){
     char *current = malloc(len*sizeof(char));
     int letter;
     int counter = 0;
+    int skip = 0;
     letter = fgetc(inFile);
     do {
         if (counter > len){
-            counter = 0;
+            // counter = 0;
+
             if (!strcmp(current, replacee)){
+                // printf("%s\n",current );
                 fprintf(outFile, "%s", replacer);
                 rearrange(current, letter, len);
+                counter-=1;
             }
             else{
-                fprintf(outFile, "%s", current);
-                memset(current, 0, len);
+                printf("SKIP %d\n",skip );
+                skip++;
+                if (skip == 2){
+                    printf("lol%s\n",current );
+                    fprintf(outFile, "%s", current);
+                    skip = 0;
+                }
+                rearrange(current, letter, len);
+                counter -=1;
             }
         }
+
         *(current + counter) = letter;
+        printf("%s\n",current );
         letter = fgetc(inFile);
         counter++;
+        // printf("%c\n",letter);
 
     } while (letter != EOF);
+    fprintf(outFile, "\n");
     free(current);
     fclose(outFile);
 }
