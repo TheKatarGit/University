19f7eda394d76678f311e5a20a14e346d54c27ba
TheKatarGit <d.mihalkovich@gmail.com>
2016-10-31 21:17:52 +0300

    Initial commit

---
+++ C Programming/Lab 5/ISOlib.c
@@ -0,0 +1,13 @@
+#include "ISOlib.h"
+
+void remove_extra_spaces(char *string, int string_length) {
+    /* code */
+}
+
+void replace_commas(char *string, int string_length) {
+    /* code */
+}
+
+void capitalize_beginning(char *string, int string_length) {
+    /* code */
+}
---
+++ C Programming/Lab 5/ISOlib.h
@@ -0,0 +1,6 @@
+
+void remove_extra_spaces(char *string, int string_length);
+
+void replace_commas(char *string, int string_length);
+
+void capitalize_beginning(char *string, int string_length);
