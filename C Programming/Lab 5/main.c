9becac775650246093983789d48eec3cbe3f6f0c
TheKatarGit <d.mihalkovich@gmail.com>
2016-11-01 17:49:49 +0300

    Working over comma rearrangement

--- C Programming/Lab 5/main.c
+++ C Programming/Lab 5/main.c
@@ -1,6 +1,12 @@
 #include "ISOlib.h"
 
 int main(int argc, char const *argv[]) {
-    
+    char string[36] = "   why     no ,u suck        my.dick";
+    printf("%s\n", remove_extra_spaces(string, 36));
+    // char *blah2 = replace_commas(blah, 36);
+    // char *blah3 = capitalize_beginning(blah2, 36);
+    // char *blah4 = remove_extra_spaces(blah3, 36);
+    // capitalize_beginning(string, 36);
+    // printf("%s\n",string);
     return 0;
 }
