3432f9f0a34bd1cca7acb09efcb33ce4b895ceef
TheKatarGit <d.mihalkovich@gmail.com>
2016-11-11 15:16:26 +0300

    Legacy removed

--- C Programming/Lab 4/Lab 4 part 2/flowlib.c
+++ C Programming/Lab 4/Lab 4 part 2/flowlib.c
@@ -1,33 +1,17 @@
 #include "flowlib.h"
 
-void rearrange(char *array, int letter, int len) {
-    if (len == 0){
-        *array = letter;
-        return;
-    }
-    for (size_t i = 0; i <= len - 1; i++) {
-        *(array + i) = *(array + i + 1);
-
-    }
-    // *(array + len) = letter;
-}
-
 int go_through(FILE *inFile, const char *replacee, const char *replacer){
     FILE *outFile;
     fpos_t pos;
     outFile = fopen("out.txt", "w");
-    if (outFile == NULL) {
-        return -1;
-    }
+    if (outFile == NULL) {return -1;}
     int len = strlen(replacee);
     char *current = malloc(len*sizeof(char));
-    char *res;
     int counter = len;
-    int skip = 0;
     fgetpos(inFile, &pos);
     fgets(current,len+1,inFile);
     do {
-        if (counter == len){
+        if (counter == len) {
             if (!strcmp(current, replacee)){
                 counter = 0;
                 fprintf(outFile, "%s", replacer);
--- C Programming/Lab 4/Lab 4 part 2/main.c
+++ C Programming/Lab 4/Lab 4 part 2/main.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]) {
     }
     inFile = fopen(argv[1], "r");
     if (!inFile) {printf("Can't open file\n"); return 0;};
-    if ( go_through(inFile, argv[2], argv[3]) == -1) {printf("Can't open file\n"); return 0;};
+    if (go_through(inFile, argv[2], argv[3]) == -1) {printf("Can't open file\n"); return 0;};
     fclose(inFile);
     return 0;
 }
