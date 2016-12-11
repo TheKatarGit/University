07cfc4c1976a9103faafba997584b2ac4eaff6d2
TheKatarGit <d.mihalkovich@gmail.com>
2016-12-11 19:15:40 +0300

    Fill list func implemented

--- C Programming/Lab 6/list.c
+++ C Programming/Lab 6/list.c
@@ -1,6 +1,23 @@
-void fill_list(struct Mylist_node *start,FILE* f){
-  while (!feof(f)) {
-      // start->data->model, 
-  }
 
+struct list_node* create_node(char *model, int price){
+    struct list_node *node = malloc(sizeof(struct list_node))
+    if (node){
+        strcpy(node->data->model, model);
+        node->data->price = price;
+        node->next = NULL;
+    }
+    return node;
+}
+
+
+
+void fill_list(struct Mylist_node *start,FILE* f){
+  char model[20];
+  int price;
+  while (!feof(f)) {
+      fscanf(f, "%s", &model);
+      fscanf(f, "%d", &price);
+      struct MyList_node *node = create_node(model, price);
+      start->next = node;
+  }
 }
