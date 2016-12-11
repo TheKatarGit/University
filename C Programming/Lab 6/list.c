bbaf44b2f67229e585ee3b1fe50f4819c554192c
Egorbigboss <egorbigboss@gmail.com>
2016-12-11 19:14:25 +0300

    Function print_list added

--- C Programming/Lab 6/list.c
+++ C Programming/Lab 6/list.c
@@ -1,6 +1,14 @@
 void fill_list(struct Mylist_node *start,FILE* f){
   while (!feof(f)) {
-      // start->data->model, 
+      // start->data->model,
   }
 
+}
+
+void print_list(struct Mylist_node *start){
+  printf("List:\n");
+  for ( ; start; start = start->next){
+			printf("\nModel - %s\nPrice - %d\n\n",start->data->model,start->data->price);
+	}
+  printf("\n");
 }
