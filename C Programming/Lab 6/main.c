d6f337e8a4c0a8ceccec0ff7d249c3f2491a1cd9
TheKatarGit <d.mihalkovich@gmail.com>
2016-12-11 19:29:11 +0300

    Squash commit

--- C Programming/Lab 6/main.c
+++ C Programming/Lab 6/main.c
@@ -1,25 +1,27 @@
+#include "list.h"
+
 int main(int argc, char const *argv[]) {
   if (argc != 2) {
     printf("Insufficient arguments\n");
     return 0;
   }
-  MyList_node *start;
-  FILE *f = fopen(argv[1]);
+  struct Mylist_node *start;
+  FILE *f = fopen(argv[1], "r");
   if (!f) {
     printf("Couldn't open file\n");
     return 0;
   }
-  fill_list(start, f);
-  fclose(f);
-  print_list(start);
-  pop_front(start);
-  print_list(start);
-  pop_end(start);
-  print_list(start);
-  reverse(start);
-  print_list(start);
-  insert_sort(start);
-  print_list(start);
-  free_list(start);
+  // fill_list(start, f);
+  // fclose(f);
+  // print_list(start);
+  // pop_front(start);
+  // print_list(start);
+  // pop_end(start);
+  // print_list(start);
+  // reverse(start);
+  // print_list(start);
+  // insert_sort(start);
+  // print_list(start);
+  // free_list(start);
   return 0;
 }
